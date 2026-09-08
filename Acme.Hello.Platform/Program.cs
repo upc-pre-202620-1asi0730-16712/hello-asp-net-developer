506d857110dcb0bdbdcced146994b7bb543907e3
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-08 09:50:26 -0500

    feat: add Grettings endpoints

--- Acme.Hello.Platform/Program.cs
+++ Acme.Hello.Platform/Program.cs
@@ -1,3 +1,7 @@
+using Acme.Hello.Platform.Generic.Domain.Model.Entities;
+using Acme.Hello.Platform.Generic.Interfaces.REST.Assemblers;
+using Acme.Hello.Platform.Generic.Interfaces.REST.Resources;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +15,30 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+
+app.MapGet("/", () => "Hello World!");
+
+// Greetings endpoint 
+app.MapGet("/greetings", (string? firstName, string? lastName) =>
+{
+    var developer = !string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName)
+        ? new Developer(firstName, lastName)
+        : null;
+    var response = GreetDeveloperAssembler.ToResponseFromEntity(developer);
+    return Results.Ok(response);
+})
+.WithName("GetGreetings")
+.WithOpenApi();
+
+app.MapPost("/greetings", (GreetDeveloperRequest? request) =>
+{
+    var developer = DeveloperAssembler.ToEntityFromRequest(request);
+    var response = GreetDeveloperAssembler.ToResponseFromEntity(developer);
+    return Results.Created("/greetings", response);
+})
+.WithName("CreateGreetings")
+.WithOpenApi();
 
 app.UseHttpsRedirection();
 
